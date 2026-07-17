f49cdec39028c97c1c5c614ee5bdbc3ca188ccfc
manikandan-n-07 <manikandan-n-07@users.noreply.github.com>
2026-07-17 17:07:58 +0000

    deploy: web update from 130e72e8c977d62daa5574acff69b83d54135c38 130e72e8c977d62daa5574acff69b83d54135c38

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2880548180" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "47627929" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
